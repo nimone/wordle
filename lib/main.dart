0cbda10a6fbd5f7b99c5a23150c6706f3d2e1a2f
nimone <nishantmogha1234@gmail.com>
2022-01-19 10:40:24 +0530

    random words on every new game

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wordle/models/board_model.dart';
 import 'package:wordle/widgets/alert_dialog.dart';
 import 'package:wordle/widgets/board.dart';
-import 'package:wordle/widgets/character_box.dart';
 
 void main(List<String> args) {
   runApp(
@@ -21,14 +24,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late final String targetWord;
-  late final BoardModel board;
-
-  @override
-  void initState() {
-    super.initState();
-    targetWord = "count";
-    board = BoardModel(targetWord, rows: targetWord.length + 1);
+  Future<String> getRandomWord() async {
+    final words = jsonDecode(
+      await rootBundle.loadString("assets/json/words.json"),
+    )["5"];
+    return words[Random().nextInt(words.length)];
   }
 
   @override
@@ -39,56 +39,65 @@ class _MyAppState extends State<MyApp> {
         centerTitle: true,
         title: const Text("Wordle"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SingleChildScrollView(
-            child: GameBoard(
-              board: board,
-              onWin: () => showAlertDialog(
-                context,
-                title: "You Guessed The Word!",
-                actionText: "Start New Game?",
-                onAction: () => setState(board.reset),
-                content: [
-                  Text(
-                    board.targetWord.toUpperCase(),
-                    style: const TextStyle(
-                      fontSize: 32,
-                      color: Colors.green,
-                      fontWeight: FontWeight.bold,
+      body: FutureBuilder(
+          future: getRandomWord()
+              .then((word) => BoardModel(word, rows: word.length + 1)),
+          builder: (context, AsyncSnapshot<BoardModel> snapshot) {
+            if (snapshot.data == null) return const Text("Cannot load game");
+
+            final board = snapshot.data!;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SingleChildScrollView(
+                  child: GameBoard(
+                    board: board,
+                    onWin: () => showAlertDialog(
+                      context,
+                      title: "You Guessed The Word!",
+                      actionText: "Start New Game?",
+                      onAction: () => setState(board.reset),
+                      content: [
+                        Text(
+                          board.targetWord.toUpperCase(),
+                          style: const TextStyle(
+                            fontSize: 32,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                            "in ${board.currentRow + 1}/${board.rows} guesses"),
+                      ],
+                    ),
+                    onLose: () => showAlertDialog(
+                      context,
+                      title: "The Secret Word was",
+                      actionText: "Try Another Word?",
+                      onAction: () => setState(board.reset),
+                      content: [
+                        Text(
+                          board.targetWord.toUpperCase(),
+                          style: const TextStyle(
+                            fontSize: 32,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  Text("in ${board.currentRow + 1}/${board.rows} guesses"),
-                ],
-              ),
-              onLose: () => showAlertDialog(
-                context,
-                title: "The Secret Word was",
-                actionText: "Try Another Word?",
-                onAction: () => setState(board.reset),
-                content: [
-                  Text(
-                    board.targetWord.toUpperCase(),
-                    style: const TextStyle(
-                      fontSize: 32,
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton.icon(
-            onPressed: () => setState(board.reset),
-            icon: const Icon(Icons.play_arrow_rounded),
-            label: const Text("New Game"),
-          ),
-        ],
-      ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () => setState(board.reset),
+                  icon: const Icon(Icons.play_arrow_rounded),
+                  label: const Text("New Game"),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
